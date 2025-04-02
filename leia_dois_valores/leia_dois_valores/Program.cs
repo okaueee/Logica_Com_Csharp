616b58df2bf43a76dcdec366b425349db694453d
okaueee <ko1400428@gmail.com>
2025-04-02 10:55:54 -0300

    logica_programa

---
+++ leia_dois_valores/leia_dois_valores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leia_dois_valores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Digite o primeiro numero: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo numero: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                c = a + b;
+                Console.WriteLine("Resultado: " + c);
+            }
+            else
+            {
+                c = a * b;
+                Console.WriteLine("Resultado: " + c);
+            }
+        }
+    }
+}
---
+++ leia_dois_valores/leia_dois_valores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("leia_dois_valores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("leia_dois_valores")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("2ab9f2c4-7cae-4717-9497-f0ac9c315a43")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

e6b5188336fba8f3bbfac0b733fa2e95a8bc458f
okaueee <ko1400428@gmail.com>
2025-04-02 10:58:26 -0300

    logica_programa

---
+++ salario_minimo/salario_minimo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salario_minimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioMinimo = 1518, salarioUsuario, qtdSalarioMinimo;
+
+            Console.Write("Digite o salario: ");
+            salarioUsuario = double.Parse(Console.ReadLine());
+
+            qtdSalarioMinimo = (int)(salarioUsuario / salarioMinimo);
+            Console.WriteLine("Salario usuario: " + salarioUsuario + "\nSalario minimo " + salarioMinimo + "\nQuantidade de salario minimo: " + qtdSalarioMinimo);
+
+        }
+    }
+}
+    
+

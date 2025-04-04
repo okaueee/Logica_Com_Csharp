0d8d4a811fc14250ba98308d698c51feb5bd72b1
okaueee <ko1400428@gmail.com>
2025-04-04 11:56:26 -0300

    logica_programaçao

---
+++ cadastre_dados_cliente/cadastre_dados_cliente/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastre_dados_cliente
+{
+    internal class Program
+    {
+        //Crie um sistema de cadastro para um skate parque, que possui duas pistas distintas, 
+        // pista adulta para maiores de 18 anos, e pista infantil para menos de 18 anos e que é obrigatório o uso de equipamento de segurança.
+        static void Main(string[] args)
+        {
+            int  dezoito_anos;
+            string nome;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("escreva seu nome :");
+            Console.ResetColor();
+            nome = Console.ReadLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("escreva sua idade:");                                                         
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.ResetColor();
+            dezoito_anos = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            if (dezoito_anos >= 18)
+            {
+                
+                Console.WriteLine(" voce não pode usar pista infantil, procure e reserve a de adulto!");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("===================================================================");
+            }
+            else 
+             { 
+            
+                Console.WriteLine("voce pode usar pista infantil,usando os equipamentos de proteção!!");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("===================================================================");
+
+            }
+               
+                
+            }
+        }
+        
+    }
+

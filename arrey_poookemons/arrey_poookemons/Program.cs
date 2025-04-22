23e95cfcee575c8183c7bedbdd6a153666d55616
okaueee <ko1400428@gmail.com>
2025-04-22 11:35:37 -0300

    logica_programaçao

---
+++ arrey_poookemons/arrey_poookemons/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrey_poookemons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    string[] pokemon = { "Bulbassaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata", "Ekans", "Pikachu", "Nidoran" };
+                    string[] tipo = { "Planta", "Fogo ", "Água ", "Bug ", "Bug ", "Normal", "Normal", "Poison", "Eletric", "Poison" };
+                    double[] tamanho = { 0.7, 0.6, 0.5, 0.3, 0.3, 0.3, 0.3, 2.0, 0.4, 0.4 };
+                    double[] peso = { 6.9, 8.5, 9.0, 2.9, 3.2, 1.8, 3.5, 6.9, 6.0, 7.0 };
+
+
+
+                    Console.WriteLine(" Nome do Pokemon: " + pokemon[0]);
+                    Console.WriteLine(" Tipo: " + tipo[0]);
+                    Console.WriteLine(" Tamanho: " + tamanho[0]);
+                    Console.WriteLine(" Peso: " + peso[0]);
+
+                    Console.WriteLine(" Total de Pokemons: " + pokemon.Length);
+
+
+                    Console.WriteLine(" LISTAGEM DE POKEMONS");
+                    Console.WriteLine("ID\tNOME\t\tTIPO\t\t\tTAMANHO\t\t\t\tPESO");
+                    for (int i = 0; i < pokemon.Length; i++)
+                    {
+                        Console.Write(i);
+                        Console.Write("\t");
+                        Console.Write(pokemon[i]);
+                        Console.Write("\t\t");
+                        Console.Write(tipo[i]);
+                        Console.Write("\t\t\t");
+                        Console.Write(tamanho[i]);
+                        Console.Write("\t\t\t\t");
+                        Console.WriteLine(peso[i]);
+                    }
+
+
+
+                }
+            }
+        }
+    }
+    }
+
---
+++ jogador/jogador/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogador
+{
+    internal class Program
+    {
+        public class Ficha
+        {
+            public string nomedojogador;
+            public string nomedopersonagem;
+            public string raca;
+            public string classe;
+            public int vida;
+
+
+            public void ImprimirFicha()
+            {
+                Console.WriteLine(" \n");
+                Console.WriteLine("=========================");
+                Console.WriteLine("=====Ficha do personagem=====");
+                Console.WriteLine("Nome do personagem : " + nomedopersonagem);
+                Console.WriteLine("raça : "+ raca);
+                Console.WriteLine("classe :  " + classe);
+                Console.WriteLine("vida " + vida);
+                Console.WriteLine("=======================================");
+                Console.WriteLine("========================================");
+                Console.WriteLine("\n");
+
+            }
+
+            static void Main(string[] args)
+            {
+                Random random = new Random();
+                int d6 = random.Next(1, 6);
+                int d6jogador = 0;
+                int d6inimigo = 0;
+
+                Ficha personagemPrincipal = new Ficha();
+                personagemPrincipal.nomedojogador = "Kaue";
+                personagemPrincipal.nomedopersonagem = "silva";
+                personagemPrincipal.raca = "elfo";
+                personagemPrincipal.classe = "guereiro ";
+                personagemPrincipal.vida = 100;
+
+
+                Console.WriteLine("===== AVENTURA COMEÇA =======");
+                personagemPrincipal.ImprimirFicha();
+
+                Console.WriteLine("Voce esta bebendo em uma taverna.");
+                Console.WriteLine("Uma pessoa esbarra em voce. o que voce faz ?\n [ 1 ] se vira ? \n [ 2 ]  continua como se nada fosse");
+
+                string decisao = Console.ReadLine();
+
+                if (decisao == "1")
+                {
+                    Console.WriteLine("\n\n");
+                    Console.WriteLine("agora começa!!!!");
+                    Ficha inimigo = new Ficha();
+                    inimigo.nomedojogador = "NPC";
+                    inimigo.nomedopersonagem = "baltazar";
+                    inimigo.raca = "ORC";
+                    inimigo.classe = "ladrao";
+                    inimigo.vida = 100;
+                    inimigo.ImprimirFicha();
+
+
+                    while (inimigo.vida > 0 && personagemPrincipal.vida > 0)
+
+                    {
+                        d6jogador = random.Next(1, 6);
+                        d6inimigo = random.Next(1, 6);
+
+                        if (d6jogador > d6inimigo)
+                        {
+                            inimigo.vida -= 10;
+                            Console.WriteLine("Boa, consegui acertar");
+                        }
+                        else if (d6jogador < d6inimigo)
+                        {
+                            personagemPrincipal.vida -= 10;
+                            Console.WriteLine("Eita, essa doeu!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("nada acontece");
+                        }
+
+                        {
+                          else
+                        }
+                        Console.WriteLine("Voce continua a beber");
+                        {
+                            
+                        }
+                        
+                          
+                        
+
+                    }
+
+                      
+
+
+
+
+            }
+        }
+    }
+

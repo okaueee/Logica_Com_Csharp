a6fa15aaf21df85a946e0a0d57d88b21c5fb6a90
okaueee <ko1400428@gmail.com>
2025-04-25 09:28:09 -0300

    logica_programaçao

---
+++ calculo_media_array/calculo_media_array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_media_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+                double[] notas = new double[30];
+                double soma = 0;
+
+
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.WriteLine($"Digite a nota do aluno {i + 1}: ");
+                    notas[i] = Convert.ToDouble(Console.ReadLine());
+                    soma += notas[i];
+                }
+
+
+                double media = soma / 30;
+
+
+                Console.WriteLine($"A média das notas dos 30 alunos é: {media:F2}");
+            }
+        }
+    }
+
+
+    
+
---
+++ media_alunos_/media_alunos_/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_alunos_
+{
+    internal class Program
+    {
+        
+            
+                static void Main()
+                {
+                    
+                    double[] notas = new double[30];
+                    double soma = 0;
+
+                   
+                    for (int i = 0; i < 30; i++)
+                    {
+                        Console.WriteLine($"Digite a nota do aluno {i + 1}: ");
+                        notas[i] = Convert.ToDouble(Console.ReadLine());
+                        soma += notas[i]; 
+                    }
+
+                    
+                    double media = soma / 30;
+
+                   
+                    Console.WriteLine($"A média das notas dos 30 alunos é: {media:F2}");
+                }
+            }
+        }
+    
+
